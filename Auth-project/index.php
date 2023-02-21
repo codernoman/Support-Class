eaa8e8eb18961a18d4e7c2a42272ad8bd80faa4c
Abdullah Al Noman <codernoman@gmail.com>
2023-02-22 01:54:39 +0600

    working properly

--- Auth-project/index.php
+++ Auth-project/index.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <srcipt src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'></srcipt>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>Auth Project</title>
 </head>
 
@@ -14,10 +15,10 @@
         <div class="col-md-3"></div>
         <div class="col-md-6">
             <h1>Login page</h1>
-            <form action="login.php">
+            <form action="login.php" method="POST">
                 <!-- Email input -->
                 <div class="form-outline mb-4">
-                    <input type="email" id="email" class="form-control" name="email" />
+                    <input type="email" id="email" class="form-control" name="username" />
                     <label class="form-label" for="email">Email address</label>
                 </div>
 
@@ -29,10 +30,6 @@
 
                 <!-- Submit button -->
                 <input type="submit" class="btn btn-primary btn-block mb-4" value="Login">
-            </form>
-            <form action="login.php" method="POST">
-                
-
             </form>
         </div>
         <div class="col-md-3></div>
--- Auth-project/login.php
+++ Auth-project/login.php
@@ -1,2 +1,16 @@
 <?php
-echo "Hello";
+session_start();
+$defUserName = "noman@gmail.com";
+$defPassword = "123456";
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if($username == $defUserName && $password == $defPassword){
+        $_SESSION['username'] = $username;
+        header("Location: welcome.php");
+    }else{
+        echo "Invalid username or password";
+    }
+}
+?>
